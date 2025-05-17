c828a6089ce849cadea7e2681a8787fc7b5b5812
blvckline <aniltepeli46@gmail.com>
2025-05-18 00:27:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+loader.load('https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/main/2.0/TShirt/glTF-Binary/TShirt.glb', function(gltf) {
+  shirt = gltf.scene;
+  shirt.scale.set(1.5, 1.5, 1.5);
+  scene.add(shirt);
+});
 
